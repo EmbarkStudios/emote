76aaa5d972160e2b6be8bb51f1ee4e030e4ca127
Tom Solberg <mail@tomolsson.se>
2022-04-08 13:07:00 +0200

    fix all line endings

--- .buildkite/check-line-endings.sh
+++ .buildkite/check-line-endings.sh
@@ -1,11 +1,14 @@
 set -eo pipefail
 
 cr="$(printf "\r")"
-crlf_count=$(grep -Ilsr "${cr}$" . | wc -l)
 
-if [[ $crlf_count -gt 0 ]]; then
+any_matches=1
+grep -Ilsr "${cr}$" . || any_matches=0
+
+if [[ $any_matches -gt 0 ]]; then
     buildkite-agent annotate --style "error" --context validate-changes "Repository contains CRLF line-endings. To avoid diff issues and cross-platform issues we require that all commits are done using a LF-style. 
 
 If you're doing development on Windows, use \`git config --global core.autocrlf true\` to let Git fix this for you on commit."
     exit 1
 fi
+
