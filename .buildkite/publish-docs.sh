13689084ceb56820e1eb72e43423bfd2cdd604f2
Tom Solberg <mail@tomolsson.se>
2022-04-08 15:39:44 +0200

    more reuse

--- .buildkite/publish-docs.sh
+++ .buildkite/publish-docs.sh
@@ -1,25 +1,7 @@
 set -eo pipefail
 
-echo --- Installing dependencies
 
-apt-get update \
-    && apt-get install --no-install-recommends -y \
-        curl \
-        build-essential
-
-export PYTHONUNBUFFERED=1 \
-    PYTHONDONTWRITEBYTECODE=1 \
-    PIP_NO_CACHE_DIR=off \
-    PIP_DISABLE_PIP_VERSION_CHECK=on \
-    PIP_DEFAULT_TIMEOUT=100 \
-    POETRY_VERSION=1.2.0b1 \
-    POETRY_HOME="/tmp/poetry" \
-    POETRY_VIRTUALENVS_IN_PROJECT=true \
-    POETRY_NO_INTERACTION=1 \
-
-pip install poetry==1.2.0b1
-poetry install
-poetry env info --path
+source .buildkite/install-repo.sh
 
 echo --- Initializing gcloud
 
