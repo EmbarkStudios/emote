2f56ff298eb78b8c69983419771aa5fe8553c040
Tom Solberg <me@sbg.dev>
2022-04-09 18:51:59 +0200

    use a custom leaner base-image

---
+++ .buildkite/build-ci-runner.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+GCR_PATH=gcr.io/embark-shared/ml/ci-runner
+
+buildah build-using-dockerfile \
+        --tag $GCR_PATH \
+        --file Dockerfile \
+		--pull-always \
+        --layers \
+        --format=docker \
+		--squash \
+        .
+
+buildah push --digestfile digest.txt --format=v2s2 $GCR_PATH
+echo "🚀 CI base image pushed: \`$GCR_PATH@`cat digest.txt`\`"
