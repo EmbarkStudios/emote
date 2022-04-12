lunar lander now training with the correct loss sign.

emote/nn/gaussian_policy.py
@@ -12,37 +12,11 @@ from torch import Tensor
 from emote.nn.initialization import ortho_init_
 
 
-class SquashStretchTransform(transforms.Transform):
-    r"""
-    Transform via the mapping :math:`y = \alpha \tanh(x/\alpha)`.
-    """
-    domain = transforms.constraints.real
-    bijective = True
-    sign = +1
-
-    def __init__(self, tanh_stretch_factor):
-        super().__init__()
-        self.codomain = transforms.constraints.interval(
-            -tanh_stretch_factor, tanh_stretch_factor
-        )
-        self._stretch = tanh_stretch_factor
-
-    @staticmethod
-    def atanh(x):
-        return 0.5 * (x.log1p() - (-x).log1p())
-
-    def _call(self, x):
-        return self._stretch * torch.tanh(x / self._stretch)
-
+class RobustTanhTransform(transforms.TanhTransform):
     def _inverse(self, y):
         eps = torch.finfo(y.dtype).eps
-        input_val = (y / self._stretch).clamp(min=-1.0 + eps, max=1.0 - eps)
-        return self._stretch * self.atanh(input_val)
-
-    def log_abs_det_jacobian(self, x, y):
-        return 2.0 * (
-            math.log(2.0) - x / self._stretch - F.softplus(-2.0 * x / self._stretch)
-        )
+        input_val = y.clamp(min=-1.0 + eps, max=1.0 - eps)
+        return torch.atanh(input_val)
 
 
 class BasePolicy(nn.Module):
@@ -68,12 +42,10 @@ class GaussianPolicyHead(nn.Module):
         self,
         hidden_dim: int,
         action_dim: int,
-        tanh_stretch_factor: float = 1.0,
     ):
         super(GaussianPolicyHead, self).__init__()
         self.action_dim = action_dim
         self.hidden_dim = hidden_dim
-        self.squash = SquashStretchTransform(tanh_stretch_factor)
         self.mean = nn.Linear(hidden_dim, action_dim)
         self.log_std = nn.Linear(hidden_dim, action_dim)
 
@@ -87,12 +59,12 @@ class GaussianPolicyHead(nn.Module):
         """
         bsz, _ = x.shape
 
-        mean = self.mean(x)
+        mean = self.mean(x).clamp(min=-5, max=5)  # equates to 0.99991 after tanh.
         std = torch.exp(self.log_std(x).clamp(min=-20, max=2))
 
         dist = dists.TransformedDistribution(
             dists.Independent(dists.Normal(mean, std), 1),
-            self.squash,
+            RobustTanhTransform(),
         )
         sample = dist.rsample()
         log_prob = dist.log_prob(sample).view(bsz, 1)

emote/sac.py
@@ -251,12 +251,14 @@ class AlphaLoss(LossCallback):
         self.ln_alpha = ln_alpha  # This is log(alpha)
 
     def loss(self, observation):
-        _p_sample, logp_pi = self.policy(**observation)
-        # alpha_loss = -torch.mean(self.ln_alpha * (logp_pi + self.t_entropy).detach())
-        alpha_loss = torch.mean(self.ln_alpha * (-logp_pi - self.t_entropy).detach())
-        # alpha_loss = torch.mean(self.ln_alpha * (logp_pi - self.t_entropy).detach())
+        with torch.no_grad():
+            _, logp_pi = self.policy(**observation)
+            entropy = -logp_pi
+            error = entropy - self.t_entropy
+        alpha_loss = torch.mean(self.ln_alpha * error.detach())
         assert alpha_loss.dim() == 0
         self.log_scalar("loss/alpha_loss", alpha_loss)
+        self.log_scalar("training/entropy", torch.mean(entropy).item())
         return alpha_loss
 
     def end_batch(self):

experiments/emit/train_emit.py
@@ -33,7 +33,7 @@ def parse_input_args():
     parser.add_argument("--emit_dir", type=str, required=True)
     parser.add_argument("--env_config", type=str, default="WasabiEnv")
     parser.add_argument("--rl_config", type=str, default="WasabiEmoteMlp")
-    parser.add_argument("--log_dir", type=str, default="/mnt/mllogs/emote")
+    parser.add_argument("--log_dir", type=str, default="/mnt/mllogs/emote/emit")
     parser.add_argument("--device_id", type=int, default=0)
     parser.add_argument("--cpu", action="store_true")
     parser.add_argument("--infer", action="store_true")
@@ -111,12 +111,8 @@ class Policy(nn.Module):
         self.policy = GaussianPolicyHead(hidden_dims[-1], num_actions)
 
         self.encoder.apply(ortho_init_)
-
-        # self.policy.apply(ortho_init_)
-        # self.policy.mean.apply(partial(xavier_uniform, gain=0.01))
-        # self.policy.log_std.apply(partial(xavier_uniform, gain=0.01))
-        self.policy.mean.apply(partial(ortho_init_, gain=1))
-        self.policy.log_std.apply(partial(ortho_init_, gain=1))
+        # self.policy.apply(partial(ortho_init_, gain=1))
+        self.policy.apply(partial(xavier_uniform, gain=0.01))
 
     def forward(self, obs):
         return self.policy(self.encoder(obs))

experiments/lunar_lander.py
@@ -1,3 +1,6 @@
+import argparse
+from functools import partial
+import numpy as np
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from torch import nn
@@ -9,7 +12,7 @@ import time
 from emote import Trainer
 from emote.callbacks import FinalLossTestCheck, TensorboardLogger
 from emote.nn import GaussianPolicyHead
-from emote.nn.initialization import ortho_init_
+from emote.nn.initialization import ortho_init_, xavier_uniform
 from emote.memory.builder import DictObsNStepTable
 from emote.sac import (
     QLoss,
@@ -24,58 +27,72 @@ from tests.gym import DictGymWrapper
 
 
 class QNet(nn.Module):
-    def __init__(self, num_obs, num_actions, num_hidden):
+    def __init__(self, num_obs, num_actions, hidden_dims):
         super().__init__()
-        self.q = nn.Sequential(
-            nn.Linear(num_obs + num_actions, num_hidden),
-            nn.ReLU(),
-            nn.Linear(num_hidden, num_hidden),
-            nn.ReLU(),
-            nn.Linear(num_hidden, 1),
+        all_dims = [num_obs + num_actions] + hidden_dims
+
+        self.encoder = nn.Sequential(
+            *[
+                nn.Sequential(nn.Linear(n_in, n_out), nn.ReLU())
+                for n_in, n_out in zip(all_dims, hidden_dims)
+            ],
         )
-        self.q.apply(ortho_init_)
+        self.encoder.apply(ortho_init_)
+
+        self.final_layer = nn.Linear(hidden_dims[-1], 1)
+        self.final_layer.apply(partial(ortho_init_, gain=1))
 
     def forward(self, action, obs):
         x = torch.cat([obs, action], dim=1)
-        return self.q(x)
+        return self.final_layer(self.encoder(x))
 
 
 class Policy(nn.Module):
-    def __init__(self, num_obs, num_actions, num_hidden):
+    def __init__(self, num_obs, num_actions, hidden_dims):
         super().__init__()
-        self.pi = nn.Sequential(
-            nn.Linear(num_obs, num_hidden),
-            nn.ReLU(),
-            nn.Linear(num_hidden, num_hidden),
-            nn.ReLU(),
-            GaussianPolicyHead(num_hidden, num_actions),
+        self.encoder = nn.Sequential(
+            *[
+                nn.Sequential(nn.Linear(n_in, n_out), nn.ReLU())
+                for n_in, n_out in zip([num_obs] + hidden_dims, hidden_dims)
+            ],
         )
-        self.pi.apply(ortho_init_)
+        self.policy = GaussianPolicyHead(
+            hidden_dims[-1],
+            num_actions,
+        )
+
+        self.encoder.apply(ortho_init_)
+        self.policy.apply(partial(xavier_uniform, gain=0.01))
 
     def forward(self, obs):
-        return self.pi(obs)
+        sample, log_prob = self.policy(self.encoder(obs))
+        # TODO: Investigate the log_prob() logic of the pytorch distribution code.
+        # The change below shouldn't be needed but significantly improves training
+        # stability when training lunar lander.
+        log_prob = log_prob.clamp(min=-2)
+        return sample, log_prob
 
 
-def test_lunar_lander():
+def train_lunar_lander(args):
     device = torch.device("cuda")
 
-    experiment_name = "lunar_lander_test_" + str(time.time())
-
-    hidden_layer = 256
+    hidden_dims = [256, 256]
     batch_size = 2000
-    rollout_len = 20
     n_env = 10
-    learning_rate = 5e-3
     max_grad_norm = 1
+
+    rollout_len = 20
+    init_alpha = 1.0
+    learning_rate = 8e-3
 
     env = DictGymWrapper(AsyncVectorEnv([_make_env(i) for i in range(n_env)]))
     table = DictObsNStepTable(
         spaces=env.dict_space,
-        use_terminal_column=True,
+        use_terminal_column=False,
         maxlen=4_000_000,
         device=device,
     )
-    memory_proxy = TableMemoryProxy(table, use_terminal=True)
+    memory_proxy = TableMemoryProxy(table, use_terminal=False)
     dataloader = MemoryLoader(
         table, batch_size // rollout_len, rollout_len, "batch_size"
     )
@@ -83,11 +100,11 @@ def test_lunar_lander():
     num_actions = env.dict_space.actions.shape[0]
     num_obs = list(env.dict_space.state.spaces.values())[0].shape[0]
 
-    q1 = QNet(num_obs, num_actions, hidden_layer)
-    q2 = QNet(num_obs, num_actions, hidden_layer)
-    policy = Policy(num_obs, num_actions, hidden_layer)
+    q1 = QNet(num_obs, num_actions, hidden_dims)
+    q2 = QNet(num_obs, num_actions, hidden_dims)
+    policy = Policy(num_obs, num_actions, hidden_dims)
 
-    ln_alpha = torch.tensor(1.0, requires_grad=True, device=device)
+    ln_alpha = torch.tensor(np.log(init_alpha), requires_grad=True, device=device)
     agent_proxy = FeatureAgentProxy(policy, device=device)
 
     q1 = q1.to(device)
@@ -117,9 +134,10 @@ def test_lunar_lander():
         AlphaLoss(
             pi=policy,
             ln_alpha=ln_alpha,
-            opt=Adam([ln_alpha]),
+            opt=Adam([ln_alpha], lr=learning_rate),
             n_actions=num_actions,
             max_grad_norm=max_grad_norm,
+            max_alpha=10.0,
         ),
         QTarget(
             pi=policy,
@@ -127,9 +145,9 @@ def test_lunar_lander():
             q1=q1,
             q2=q2,
             roll_length=rollout_len,
+            reward_scale=0.1,
         ),
-        # ThreadedGymCollector(
-        SimpleGymCollector(
+        ThreadedGymCollector(
             env,
             agent_proxy,
             memory_proxy,
@@ -139,7 +157,13 @@ def test_lunar_lander():
     ]
 
     callbacks = logged_cbs + [
-        TensorboardLogger(logged_cbs, SummaryWriter("runs/" + experiment_name), 100),
+        TensorboardLogger(
+            logged_cbs,
+            SummaryWriter(
+                log_dir=args.log_dir + "/" + args.name + "_{}".format(time.time())
+            ),
+            100,
+        ),
         FinalLossTestCheck([logged_cbs[2]], [10.0], 50_000_000),
     ]
 
@@ -159,4 +183,9 @@ def _make_env(rank):
 
 
 if __name__ == "__main__":
-    test_lunar_lander()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", type=str, default="ll")
+    parser.add_argument("--log_dir", type=str, default="/mnt/mllogs/emote/lunar_lander")
+    args = parser.parse_args()
+
+    train_lunar_lander(args)