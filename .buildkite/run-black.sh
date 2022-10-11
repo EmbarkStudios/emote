0ede9b556ae94a1dcfb156c30b67b6599cf6a200
Tom Solberg <me@sbg.dev>
2022-10-11 11:55:12 +0200

    look at package and test steps

--- .buildkite/run-black.sh
+++ .buildkite/run-black.sh
@@ -8,7 +8,7 @@ EXIT_CODE=0
 pdm run black --check --diff emote > diff.txt || EXIT_CODE=$?
 
 if [ $EXIT_CODE -ne 0 ]; then
-	cat << EOF | buildkite-agent annotate --style "error" --context "eslint"
+	cat << EOF | buildkite-agent annotate --style "error" --context "black"
 :warning: Your code isn't formatted by \`black\`. Please fix the below diffs, or run \`pdm run black emote\` to automatically format it.
 
 \`\`\`diff
@@ -17,7 +17,7 @@ $(cat diff.txt)
 
 EOF
 else
-	buildkite-agent annotate "✅ Code formatted correctly " --style "success" --context "eslint"
+	buildkite-agent annotate "✅ Code formatted correctly " --style "success" --context "black"
 fi
 
 exit $EXIT_CODE
