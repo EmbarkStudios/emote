e42139a53ca884dfd63c6d2db52f67da59231504
Tom Solberg <me@sbg.dev>
2022-10-17 15:34:02 +0200

    lockdiff

--- .buildkite/validate-lockfile.sh
+++ .buildkite/validate-lockfile.sh
@@ -9,7 +9,14 @@ fi
 
 GIT_STATUS=$(git status --porcelain --untracked-files=no -- pdm.lock)
 if [ -n "$GIT_STATUS" ]; then
-    buildkite-agent annotate --style "error" --context "lockfile" ":lock: Lockfile is outdated. Please run \`pdm lock --no-update\` and commit the result."
+	lock_diff=$(diff pdm.lock)
+	cat << EOF | buildkite-agent annotate --style "error" --context "lockfile"
+":lock: Lockfile is outdated. Please run \`pdm lock --no-update\` and commit the result."
+
+\`\`\`diff
+$lock_diff
+\`\`\`
+EOF
 	exit 1
 else
     buildkite-agent annotate --style "success" --context "lockfile" ":lock: Lockfile is up to date."
