3b7f193b3d4e1e4e129892e76604fed915c7e2a9
Tom Solberg <me@sbg.dev>
2022-10-21 16:05:19 +0200

    add pre-release

--- .buildkite/check-line-endings.sh
+++ .buildkite/check-line-endings.sh
@@ -3,7 +3,9 @@ set -eo pipefail
 cr="$(printf "\r")"
 
 any_matches=1
+
 grep --exclude-dir=".git" -Ilsr "${cr}$" . || any_matches=0
+
 
 if [[ $any_matches -gt 0 ]]; then
     buildkite-agent annotate --style "error" --context validate-changes "Repository contains CRLF line-endings. To avoid diff issues and cross-platform issues we require that all commits are done using a LF-style.
