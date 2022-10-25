7c7a1c77832b414245a75639ea5b3d0598f22b73
Tom Solberg <me@sbg.dev>
2022-10-25 15:11:36 +0200

    be very specific

--- .buildkite/validate-lockfile.sh
+++ .buildkite/validate-lockfile.sh
@@ -13,16 +13,30 @@ fi
 TORCH_EXIT_CODE=0
 pdm torch lock --check || TORCH_EXIT_CODE=$?
 GIT_STATUS=$(git status --porcelain --untracked-files=no -- pdm.lock)
-if [ -n "$GIT_STATUS" ] || [ $TORCH_EXIT_CODE -ne 0 ]; then
+if [ -n "$GIT_STATUS" ] && [ $TORCH_EXIT_CODE -ne 0 ]; then
 	lock_diff=$(git diff pdm.lock)
 	cat << EOF | buildkite-agent annotate --style "error" --context "lockfile"
-:lock: Lockfile is outdated. Please run \`pdm lock --no-update && pdm torch lock\` and commit the result.
+:lock: Lockfiles are outdated. Please run \`pdm lock --no-update && pdm torch lock\` and commit the result.
 
 \`\`\`diff
 $lock_diff
 \`\`\`
 EOF
 	exit 1
+elif  [ -n "$GIT_STATUS" ] ; then
+	lock_diff=$(git diff pdm.lock)
+	cat << EOF | buildkite-agent annotate --style "error" --context "lockfile"
+:lock: Lockfile is outdated. Please run \`pdm lock --no-update\` and commit the result.
+
+\`\`\`diff
+$lock_diff
+\`\`\`
+EOF
+elif [ $TORCH_EXIT_CODE -ne 0 ]; then
+
+	cat << EOF | buildkite-agent annotate --style "error" --context "lockfile"
+:lock: Torch lockfile is outdated. Please run \`pdm torch lock\` and commit the result.
+EOF
 else
     buildkite-agent annotate --style "success" --context "lockfile" ":lock: Lockfile is up to date."
 fi
