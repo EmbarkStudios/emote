f2e907b7021fc24e10554711f65cfaae22573422
Tom Solberg <me@sbg.dev>
2022-11-05 01:00:49 +0100

    write file

--- .buildkite/install-repo.sh
+++ .buildkite/install-repo.sh
@@ -10,7 +10,7 @@ echo --- Installing dependencies
 
 
 ${PDM_COMMAND:1:-1} plugin add plugins/pdm-plugin-fetcher
-${PDM_COMMAND:1:-1} fetch -G ci | .buildkite/pdm-fetcher downsync
+${PDM_COMMAND:1:-1} fetch -G ci | .buildkite/pdm-fetcher downsync > manifest.json
 ${PDM_COMMAND:1:-1} install -d -G ci -k post_install
 ${PDM_COMMAND:1:-1} plugin add pdm-plugin-torch>=23.1.1
 ${PDM_COMMAND:1:-1} torch install cpu
