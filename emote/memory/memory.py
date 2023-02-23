10207a4ec131bc7d6a1c5457623b92a74feb71ed
Tom Solberg <tom.solberg@embark-studios.com>
2023-02-23 12:31:14 +0100

    Add memory warmup waiter (#78)

--- emote/memory/memory.py
+++ emote/memory/memory.py
@@ -12,6 +12,8 @@ from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Dict, List, Mapping, Optional, Tuple
 
+from emote.callback import Callback
+
 from ..typing import AgentId, DictObservation, DictResponse, EpisodeState
 from ..utils import TimedBlock
 from .core_types import Matrix
@@ -169,3 +171,25 @@ class MemoryLoader:
 
             data[self.size_key] = self.rollout_count * self.rollout_length
             yield {self.data_group: data, self.size_key: data[self.size_key]}
+
+
+class MemoryWarmup(Callback):
+    """A blocker to ensure memory has data.
+
+    This ensures the memory has enough data when training starts, as the memory
+    will panic otherwise. This is useful if you use an async data generator.
+
+    If you do not use an async data generator this can deadlock your training
+    loop and prevent progress.
+    """
+
+    def __init__(self, loader: MemoryLoader):
+        super().__init__()
+        self._order = 100
+        self._loader = loader
+
+    def begin_training(self):
+        import time
+
+        while not self._loader.is_ready():
+            time.sleep(0.1)
