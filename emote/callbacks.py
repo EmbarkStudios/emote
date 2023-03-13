4800333a946528552861f33e35bd54ad27336224
spetravic <petravic.s@gmail.com>
2023-03-13 16:08:16 +0100

    initial

--- emote/callbacks.py
+++ emote/callbacks.py
@@ -235,6 +235,73 @@ class TensorboardLogger(Callback):
         )
 
 
+class WBLogger(Callback):
+    """Logs the provided loggable callbacks to Weights&Biases."""
+
+    def __init__(
+        self,
+        callbacks: List[LoggingMixin],
+        log_interval: int,
+    ):
+        super().__init__(cycle=log_interval)
+        self._cbs = callbacks
+
+    def begin_training(self):
+        self._start_time = time.monotonic()
+
+    def end_cycle(self, bp_step, bp_samples):
+        import wandb
+
+        log_dict = {}
+        suffix = "bp_step"
+
+        for cb in self._cbs:
+            for k, v in cb.scalar_logs.items():
+                if suffix:
+                    k_split = k.split("/")
+                    k_split[0] = k_split[0] + "_" + suffix
+                    k = "/".join(k_split)
+
+                log_dict[k] = v
+
+            for k, v in cb.windowed_scalar.items():
+                if suffix:
+                    k_split = k.split("/")
+                    k_split[0] = k_split[0] + "_" + suffix
+                    k = "/".join(k_split)
+
+                log_dict[k] = sum(v) / len(v)
+
+            for k, v in cb.windowed_scalar_cumulative.items():
+                if suffix:
+                    k_split = k.split("/")
+                    k_split[0] = k_split[0] + "_" + suffix
+                    k = "/".join(k_split)
+
+                log_dict[f"{k}/cumulative"] = v
+
+            for k, v in cb.image_logs.items():
+                if suffix:
+                    k_split = k.split("/")
+                    k_split[0] = k_split[0] + "_" + suffix
+                    k = "/".join(k_split)
+                log_dict[k] = wandb.Image(v)
+
+            for k, (video_array, fps) in cb.video_logs.items():
+                if suffix:
+                    k_split = k.split("/")
+                    k_split[0] = k_split[0] + "_" + suffix
+                    k = "/".join(k_split)
+
+                log_dict[k] = wandb.Video(video_array, fps=fps)
+
+        time_since_start = time.monotonic() - self._start_time
+        log_dict["performance/bp_samples_per_sec"] = bp_samples / time_since_start
+        log_dict["performance/bp_steps_per_sec"] = bp_step / time_since_start
+        log_dict["log/bp_step"] = bp_step
+        wandb.log(log_dict)
+
+
 class TerminalLogger(Callback):
     """Logs the provided loggable callbacks to the python logger."""
 
