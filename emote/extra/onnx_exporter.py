d459068ea2984837b9c6c2b892f4062980150843
Tom Solberg <tom.solberg@embark-studios.com>
2023-05-26 17:31:40 +0200

    Enable setting `device` for ONNX exporter (#135)
    This is necessary for GPU training as otherwise the args will be on the
    wrong device during tracing.

--- emote/extra/onnx_exporter.py
+++ emote/extra/onnx_exporter.py
@@ -73,7 +73,7 @@ class OnnxExporter(LoggingMixin, Callback):
                       it will be created.
     :param interval: if provided, will automatically export ONNX files at this cadence.
     :param prefix: all file names will have this prefix.
-
+    :param device: if provided, will transfer the model inputs to this device before exporting.
     """
 
     __HAS_INSERTED_FILTER = False
@@ -86,6 +86,7 @@ class OnnxExporter(LoggingMixin, Callback):
         directory: str,
         interval: int | None = None,
         prefix: str = "savedmodel_",
+        device: torch.device | None = None,
     ):
         super().__init__(cycle=interval)
 
@@ -124,6 +125,8 @@ class OnnxExporter(LoggingMixin, Callback):
         self.inputs = input_shapes
         self.outputs = output_shapes
 
+        self._device = device
+
     def end_batch(self):
         self.process_pending_exports()
 
@@ -151,7 +154,11 @@ class OnnxExporter(LoggingMixin, Callback):
             args = []
 
             for _, shape in self.inputs:
-                args.append(torch.randn(1, *shape).detach())
+                arg = torch.randn(1, *shape).detach()
+                if self._device is not None:
+                    arg = arg.to(self._device)
+
+                args.append(arg)
 
             self.policy.train(False)
 
