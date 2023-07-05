faa6cfb1323025bc9ebe07eed786897d2d651df7
Tom Solberg <me@sbg.dev>
2023-07-05 17:24:03 +0200

    more hacks

--- .buildkite/publish-docs.sh
+++ .buildkite/publish-docs.sh
@@ -6,7 +6,7 @@ source .buildkite/install-repo.sh
 echo --- Building docs
 pushd docs
 EXIT_CODE=0
-${PDM_COMMAND:1:-1} run make deploy || EXIT_CODE=$?
+PDM=${PDM_COMMAND:1:-1} ${PDM_COMMAND:1:-1} run make deploy  || EXIT_CODE=$?
 
 if [ $EXIT_CODE -ne 0 ]; then
 	cat << EOF | buildkite-agent annotate --style "error" --context "sphinx"
