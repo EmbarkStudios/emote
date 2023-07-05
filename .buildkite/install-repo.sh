3acee1b50c84583a64beb97b28be465cd23c6461
Tom Solberg <me@sbg.dev>
2023-07-05 17:11:59 +0200

    hmm

--- .buildkite/install-repo.sh
+++ .buildkite/install-repo.sh
@@ -8,4 +8,5 @@ gcloud config set account monorepo-ci@embark-builds.iam.gserviceaccount.com
 echo --- Installing dependencies
 
 ${PDM_COMMAND:1:-1} install -d -G ci -k post_install
+${PDM_COMMAND:1:-1} plugin add pdm-plugin-torch>=23.1.1
 ${PDM_COMMAND:1:-1} torch install cpu
