5a1c8befe5125f374a8d8353a22c1f87e3900336
Martin Singh-Blom <martin.singh-blom@embark-studios.com>
2023-07-05 15:40:41 +0200

    Orthogonal init for conv as well (#152)

--- emote/nn/initialization.py
+++ emote/nn/initialization.py
@@ -7,6 +7,10 @@ def ortho_init_(m, gain=np.sqrt(2)):
     if isinstance(m, nn.Linear):
         nn.init.orthogonal_(m.weight.data, gain)
         nn.init.constant_(m.bias.data, 0.0)
+    if isinstance(m, nn.Conv2d):
+        nn.init.orthogonal_(m.weight.data, gain)
+        if m.bias:
+            nn.init.constant_(m.bias.data, 0.0)
 
 
 def xavier_uniform_init_(m, gain):
