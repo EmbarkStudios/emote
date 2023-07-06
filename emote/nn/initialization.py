78afe232e115889a216065e1958174d78bcf0c4b
Simona Petravic <simona.petravic@embark-studios.com>
2023-07-06 10:31:19 +0200

    Change check for bias initialization (#156)
    The current check raises RuntimeError: Boolean value of Tensor with more
    than one value is ambiguous

--- emote/nn/initialization.py
+++ emote/nn/initialization.py
@@ -9,7 +9,7 @@ def ortho_init_(m, gain=np.sqrt(2)):
         nn.init.constant_(m.bias.data, 0.0)
     if isinstance(m, nn.Conv2d):
         nn.init.orthogonal_(m.weight.data, gain)
-        if m.bias:
+        if m.bias is not None:
             nn.init.constant_(m.bias.data, 0.0)
 
 
