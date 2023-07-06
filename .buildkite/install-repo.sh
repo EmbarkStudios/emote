535601a77f91ceedfcdff7fd9626f162a211cea2
Tom Solberg <me@sbg.dev>
2023-07-06 11:48:45 +0200

    maybe

--- .buildkite/install-repo.sh
+++ .buildkite/install-repo.sh
@@ -8,8 +8,7 @@ gcloud config set account monorepo-ci@embark-builds.iam.gserviceaccount.com
 
 echo --- Installing dependencies
 
-
-${PDM_COMMAND:1:-1} plugin add plugins/pdm-plugin-fetcher
+${PDM_COMMAND:1:-1} plugin add .buildkite/fetcher
 ${PDM_COMMAND:1:-1} fetch -G ci | .buildkite/pdm-fetcher downsync > manifest.json
 ${PDM_COMMAND:1:-1} install -d -G ci -k post_install
 ${PDM_COMMAND:1:-1} plugin add pdm-plugin-torch>=23.1.1
