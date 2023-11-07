25bdde70d7555e1d0a8fb5f91595f295260b83e9
Ali Ghadirzadeh <ali.ghadirzadeh@gmail.com>
2023-11-07 17:47:48 +0100

    some updates

--- experiments/generate_json.py
+++ experiments/generate_json.py
@@ -1,34 +1,36 @@
-import numpy as np
-import os
-import torch
 import argparse
-from matplotlib import pyplot as plt
-
-from emote.memory.builder import DictObsNStepTable
-from emote.utils.spaces import BoxSpace, DictSpace, MDPSpace
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--action-count", type=int, default=51)
-    parser.add_argument("--obs-count", type=int, default=252)
+    parser.add_argument("--joint-angle", action="store_true")
+    parser.add_argument("--joint-velocity", action="store_true")
+    parser.add_argument("--position", action="store_true")
+    parser.add_argument("--linear-velocity", action="store_true")
+    parser.add_argument("--root", action="store_true")
 
     arg = parser.parse_args()
-
-    action_count = arg.action_count
-    obs_count = arg.obs_count
 
     joint_angle_idx = [[4 * k, 4 * k + 1, 4 * k + 2] for k in range(17)]
     linear_velocity_idx = [[68 + 9 * k, 68 + 9 * k + 1, 68 + 9 * k + 2] for k in range(17)]
     angular_velocity_idx = [[68 + 9 * k + 3, 68 + 9 * k + 4, 68 + 9 * k + 5] for k in range(17)]
     position_idx = [[17 * 4 + 9 * k + 6, 17 * 4 + 9 * k + 7, 17 * 4 + 9 * k + 8] for k in range(17)]
-    root_idx = [k + 239 for k in range(9)]
+    root_idx = [[3 * k + 239, 3 * k + 239 + 1, 3 * k + 239 + 2] for k in range(3)]
 
-    idx_list = linear_velocity_idx
-    for idx in idx_list:
+    list_of_indices = []
+    if arg.joint_angle:
+        list_of_indices.extend(joint_angle_idx)
+    if arg.joint_velocity:
+        list_of_indices.extend(angular_velocity_idx)
+    if arg.position:
+        list_of_indices.extend(position_idx)
+    if arg.linear_velocity:
+        list_of_indices.extend(angular_velocity_idx)
+    if arg.root:
+        list_of_indices.extend(root_idx)
+
+    for idx in list_of_indices:
         print('{')
         print(f"\t\"start\": {idx[0]},")
         print(f"\t\"end\": {idx[2]}")
         print('},')
-
-    print(root_idx)
