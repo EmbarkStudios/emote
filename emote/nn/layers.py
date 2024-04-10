d5be41b3674550e669ddaca17105b64587961ee8
Ali <ali.ghadirzadeh@embark-studios.com>
2024-04-10 18:24:32 +0200

    wip

--- emote/nn/layers.py
+++ emote/nn/layers.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
+from functools import partial
 import numpy as np
 import torch
-import torch.nn as nn
-
+import torch.nn.functional as F
+from emote.nn.gaussian_policy import GaussianPolicyHead
+from typing import Mapping, Optional, Union
+from torch import Tensor, nn
 from emote.nn.initialization import ortho_init_
+from emote.nn.gaussian_policy import GaussianMlpPolicy
 
 
 class Conv2dEncoder(nn.Module):
@@ -22,16 +26,16 @@ class Conv2dEncoder(nn.Module):
     """
 
     def __init__(
-        self,
-        input_shape: tuple[int, int, int],
-        channels: list[int],
-        kernels: list[int],
-        strides: list[int],
-        padding: list[int],
-        channels_last: bool = True,
-        activation: torch.nn.Module = torch.nn.ReLU,
-        flatten: bool = True,
-        **kwargs,
+            self,
+            input_shape: tuple[int, int, int],
+            channels: list[int],
+            kernels: list[int],
+            strides: list[int],
+            padding: list[int],
+            channels_last: bool = True,
+            activation: torch.nn.Module = torch.nn.ReLU,
+            flatten: bool = True,
+            **kwargs,
     ):
         super().__init__(**kwargs)
         self._channels_last = channels_last
@@ -107,17 +111,17 @@ class Conv1dEncoder(nn.Module):
     """
 
     def __init__(
-        self,
-        input_shape: tuple[int, int],
-        channels: list[int],
-        kernels: list[int],
-        strides: list[int],
-        padding: list[int],
-        activation: torch.nn.Module = torch.nn.ReLU,
-        flatten: bool = True,
-        name: str = "conv1d",
-        channels_last: bool = True,
-        **kwargs,
+            self,
+            input_shape: tuple[int, int],
+            channels: list[int],
+            kernels: list[int],
+            strides: list[int],
+            padding: list[int],
+            activation: torch.nn.Module = torch.nn.ReLU,
+            flatten: bool = True,
+            name: str = "conv1d",
+            channels_last: bool = True,
+            **kwargs,
     ):
         super().__init__(**kwargs)
         self._channels_last = channels_last
@@ -178,13 +182,13 @@ class Conv1dEncoder(nn.Module):
 
 class TNet(nn.Module):
     def __init__(
-        self,
-        num_points: int,
-        input_dim: int,
-        input_stack_hidden_dims: list[int] = None,
-        feature_stack_hidden_dims: list[int] = None,
-        use_batch_norm: bool = False,
-        device: torch.device = torch.device("cpu"),
+            self,
+            num_points: int,
+            input_dim: int,
+            input_stack_hidden_dims: list[int] = None,
+            feature_stack_hidden_dims: list[int] = None,
+            use_batch_norm: bool = False,
+            device: torch.device = torch.device("cpu"),
     ):
         """
         TNet mapping in PointNet architecture
@@ -233,7 +237,7 @@ class TNet(nn.Module):
             )
         ]
         feature_mlp.append(
-            nn.Sequential(nn.Linear(feature_stack_hidden_dims[-1], self._input_dim**2))
+            nn.Sequential(nn.Linear(feature_stack_hidden_dims[-1], self._input_dim ** 2))
         )
         self.input_mlp = nn.Sequential(*input_mlp).to(self._device)
         self.input_mlp.apply(ortho_init_)
@@ -253,15 +257,15 @@ class TNet(nn.Module):
 
 class PointNetEncoder(nn.Module):
     def __init__(
-        self,
-        num_points: int,
-        input_dim: int,
-        output_dim: int,
-        input_stack_hidden_dims: list[int] = None,
-        feature_stack_hidden_dims: list[int] = None,
-        use_tnet: bool = False,
-        use_batch_norm: bool = False,
-        device: torch.device = torch.device("cpu"),
+            self,
+            num_points: int,
+            input_dim: int,
+            output_dim: int,
+            input_stack_hidden_dims: list[int] = None,
+            feature_stack_hidden_dims: list[int] = None,
+            use_tnet: bool = False,
+            use_batch_norm: bool = False,
+            device: torch.device = torch.device("cpu"),
     ):
         """
         PointNet encoder class
@@ -369,3 +373,143 @@ class PointNetEncoder(nn.Module):
         # x.shape: [bs, num_points, dim_points]
         features = self.mlp_model(x.transpose(2, 1)).squeeze(-1)
         return features
+
+
+class SharedEncoderPolicy(GaussianMlpPolicy):
+    def __init__(
+            self,
+            shared_enc: nn.Module,
+            observation_dim: int,
+            action_dim: int,
+            hidden_dims: list[int],
+    ):
+        """
+        Policy class: The policy gets an encoder network that maps input observations (in this
+        case, images) into a D-dimensional representation. The policy class adds some extra MLP
+        layers, and a Guassian head to implement the final stochastic policy. Extra features can
+        be flexibly added as the policy input by the forward method which can be overridden to
+        accommodate different input observations (input keys).
+        :params shared_enc (nn.Module): The shared encoder network between the policy and the
+            q-network. Note that the shared encoder should not be trained by the policy loss
+        :observation_dim (int): The observation size (the input dimension of the MLP layers)
+        :action_dim (int): The action size (policy output size)
+        :hidden_dim (list[int]): The MLP hidden layer dimensions.
+        """
+        super().__init__(
+            observation_dim=observation_dim, action_dim=action_dim, hidden_dims=hidden_dims
+        )
+        self.shared_enc = shared_enc
+
+    def forward(
+            self, images: Tensor, features: Tensor, epsilon: Union[Tensor, None] = None, **kwargs
+    ) -> Union[Tensor, tuple[Tensor]]:
+        """
+        The forward method of the policy. This method can be overridden by the child
+        class to accommodate various observation keys.
+        """
+        enc_im = self.shared_enc(images)
+        x = torch.cat([enc_im, features], dim=1)
+        return super().forward(x, epsilon=epsilon)
+
+    def get_trainable_params(self):
+        # ** This is critical for training! **
+        # Prevent the policy loss from training the shared encoder
+
+        # IMPORTANT. When using a shared encoder, this filtering is critical. It may not seem so
+        # at first, if we use a stop_gradient after the encoder for the policy.
+        # However, because Q and P share an encoder and we have to backpropagate the policy sample
+        # though the q network, if this filtering isn't present, the policy loss actually trains the
+        # encoder indirectly through the q(obs, sample) evaluation.
+        trainable_params = list(self.mlp_encoder.parameters()) + list(self.policy.parameters())
+
+        return trainable_params
+
+
+class PointCloudPolicy(SharedEncoderPolicy):
+    def __init__(
+            self,
+            shared_enc: nn.Module,
+            observation_dim: int,
+            action_dim: int,
+            hidden_dims: list[int],
+    ):
+        super().__init__(shared_enc, observation_dim, action_dim, hidden_dims)
+
+    def forward(self, point_cloud: Tensor, features: Tensor, epsilon: Union[Tensor, None] = None):
+        return super().forward(point_cloud, features, epsilon)
+
+
+class QNet(nn.Module):
+    def __init__(
+        self,
+        observation_dim: int,
+        action_dim: int,
+        hidden_dims: list[int],
+    ):
+        super().__init__()
+        self.obs_d = observation_dim
+        self.act_d = action_dim
+        self.mlp_encoder = nn.Sequential(
+            *[
+                nn.Sequential(nn.Linear(n_in, n_out), nn.ReLU())
+                for n_in, n_out in zip(
+                    [observation_dim + action_dim] + hidden_dims,
+                    hidden_dims,
+                )
+            ],
+        )
+        self.mlp_encoder.apply(ortho_init_)
+
+        self.final_layer = nn.Linear(hidden_dims[-1], 1)
+        self.final_layer.apply(partial(ortho_init_, gain=1))
+
+    def forward(
+        self,
+        action: Tensor,
+        features: Tensor,
+    ) -> Tensor:
+        bsz, obs_d = features.shape
+        bsz_action, act_d = action.shape
+        assert bsz == bsz_action
+        assert obs_d == self.obs_d
+        assert act_d == self.act_d
+        x = torch.cat([features, action], dim=1)
+        out = self.final_layer(self.mlp_encoder(x))
+        assert (bsz, 1) == out.shape
+        return out
+
+
+class SharedEncoderQNet(QNet):
+    def __init__(
+        self,
+        shared_enc: nn.Module,
+        obs_dim: int,
+        action_dim: int,
+        hidden_dims: list[int],
+    ):
+        super().__init__(observation_dim=obs_dim, action_dim=action_dim, hidden_dims=hidden_dims)
+        self.shared_enc = shared_enc
+
+    def forward(self, action: Tensor, features: Tensor, images: Tensor, **kwargs):
+        x = self.shared_enc(images)
+        x = torch.cat([x, features], dim=1)
+        return super().forward(action, x)
+
+
+class PointCloudQNet(SharedEncoderQNet):
+    def __init__(
+        self,
+        shared_enc: nn.Module,
+        obs_dim: int,
+        action_dim: int,
+        hidden_dims: list[int],
+    ):
+        super().__init__(shared_enc, obs_dim, action_dim, hidden_dims)
+
+    def forward(
+        self,
+        action: Tensor,
+        features: Tensor,
+        point_cloud: Tensor,
+    ):
+        return super().forward(action, features, point_cloud)
--- experiments/run_pointnet_policy.py
+++ experiments/run_pointnet_policy.py
@@ -1,25 +1,36 @@
 import numpy as np
 import torch
+from torch import nn
 import argparse
-from emote.nn.layers import PointNetEncoder
-
+from emote.nn.layers import PointCloudPolicy, PointNetEncoder, PointCloudQNet
 from emote.memory.builder import DictObsNStepTable
 from emote.utils.spaces import BoxSpace, DictSpace, MDPSpace
 from emote.memory.memory import MemoryLoader
+from emote.utils.gamma_matrix import discount, make_gamma_matrix, split_rollouts
+from torch.optim import Adam
+
+from torch.profiler import profile, record_function, ProfilerActivity
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--path-to-buffer", type=str, default="/home/ali/data/biped/replay_buffer/")
-    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--device", type=str, default="cuda:0")
+    parser.add_argument("--batch", type=int, default=4000)
     parser.add_argument("--action-dim", type=int, default=22)
     parser.add_argument("--observation-dim", type=int, default=225)
     parser.add_argument("--point-count", type=int, default=300)
     parser.add_argument("--point-dim", type=int, default=3)
-    parser.add_argument("--episode-length", type=int, default=5)
+    parser.add_argument("--pointnet-output-dim", type=int, default=128)
+    parser.add_argument("--rollout-length", type=int, default=5)
     parser.add_argument("--memory-size", type=int, default=500000)
+    parser.add_argument("--policy-hidden-sizes", nargs="+", type=int, default=[1024, 1024])
+    parser.add_argument("--pointnet-input-hidden-sizes", nargs="+", type=int, default=[64, 64])
+    parser.add_argument("--pointnet-feature-hidden-sizes", nargs="+", type=int, default=[64, 128])
     parser.add_argument("--use-terminal", action='store_true')
+    parser.add_argument("--use-batch-norm", action='store_true')
+    parser.add_argument("--use-tnet", action='store_true')
 
     arg = parser.parse_args()
 
@@ -61,21 +72,96 @@ if __name__ == '__main__':
     print(f"the size of the table is: {table.size()}")
     data_loader = MemoryLoader(
         table,
-        1,
-        arg.episode_length,
+        arg.batch // arg.rollout_length,
+        arg.rollout_length,
         "batch_size",
         data_group="group",
     )
-    print('*' * 30)
+
+    pc_encoder = PointNetEncoder(
+        num_points=arg.point_count,
+        input_dim=arg.point_dim,
+        output_dim=arg.pointnet_output_dim,
+        input_stack_hidden_dims=arg.pointnet_input_hidden_sizes,
+        feature_stack_hidden_dims=arg.pointnet_feature_hidden_sizes,
+        use_batch_norm=arg.use_batch_norm,
+        use_tnet=arg.use_tnet,
+        device=device,
+    )
+    policy = PointCloudPolicy(
+        shared_enc=pc_encoder,
+        action_dim=arg.action_dim,
+        hidden_dims=arg.policy_hidden_sizes,
+        observation_dim=arg.observation_dim + arg.pointnet_output_dim,
+    )
+    qnet = PointCloudQNet(
+        shared_enc=pc_encoder,
+        action_dim=arg.action_dim,
+        hidden_dims=arg.policy_hidden_sizes,
+        obs_dim=arg.observation_dim + arg.pointnet_output_dim
+    )
+    alpha = 0.5
+    reward_scale = 1.0
+    gamma = 0.97
+    ln_alpha = torch.tensor(np.log(alpha), requires_grad=True, device=device)
+    policy = policy.to(device)
+    qnet = qnet.to(device)
+
+    q_opt = Adam(qnet.parameters(), lr=0.001)
+    p_opt = Adam(policy.parameters(), lr=0.001)
+
+    mse_loss = nn.MSELoss()
+
+    gamma_matrix = make_gamma_matrix(gamma, arg.rollout_length).to(device)
 
     itr = iter(data_loader)
     data = next(itr)
-    obs = data['group']['observation'][obs_key]
-    pc = data['group']['observation'][pc_key]
+
+    observation = data['group']['observation']
+    next_observation = data['group']['next_observation']
     actions = data['group']['actions']
+    rewards = data['group']['rewards']
+    masks = data['group']['masks']
+    with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+        with record_function("training"):
 
-    print(f"observation shape {obs.shape}")
-    print(f"point cloud shape {pc.shape}")
-    print(f"action shape {actions.shape}")
+            # compute next q
+            next_p_sample, next_log_p = policy(**next_observation)
+            min_next_qt = qnet(next_p_sample, **next_observation)
+            bsz = rewards.shape[0]
+            alpha = torch.exp(ln_alpha)
+            next_value = min_next_qt - alpha * next_log_p
+            scaled_reward = reward_scale * rewards
 
+            last_step_masks = split_rollouts(masks, arg.rollout_length)[:, -1]
+            scaled_reward = split_rollouts(scaled_reward, arg.rollout_length).squeeze(2)
 
+            if arg.use_terminal:
+                next_value = torch.multiply(next_value, last_step_masks)
+
+            q_target = discount(scaled_reward, next_value, gamma_matrix).detach()
+
+            # compute the q-loss
+            q_opt.zero_grad()
+
+            q_value = qnet(actions, **observation)
+            q_loss = mse_loss(q_value, q_target)
+
+            # optimize the q-loss
+            q_loss.backward()
+            q_opt.step()
+
+            # compute policy loss
+            p_opt.zero_grad()
+
+            p_sample, log_p = policy(**observation)
+            q_pi_min = qnet(p_sample, **observation)
+            alpha = torch.exp(ln_alpha).detach()
+            policy_loss = alpha * log_p - q_pi_min
+            policy_loss = torch.mean(policy_loss)
+
+            # optimize the policy loss
+            policy_loss.backward()
+            p_opt.step()
+
+    print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=20))
